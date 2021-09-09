665620ac90dc950b8e404d0e0aab3af750326649
Emre Tapci <emre.tapci@gmail.com>
2021-09-09 16:43:43 +0300

    .

--- migrations/2_deploy_token.js
+++ migrations/2_deploy_token.js
@@ -1,7 +1,5 @@
 const EmreToken = artifacts.require("./EmreToken");
-const EmreTokenn = artifacts.require("./EmreTokenn");
 
 module.exports = function (deployer) {
 	deployer.deploy(EmreToken, 'Emre Token', 'EMT');
-	deployer.deploy(EmreTokenn, 'Emre Tokenn', 'EMN');
 };
